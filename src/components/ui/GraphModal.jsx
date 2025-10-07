040ae415e37280cbb5bb18e5273b022abf407840
afrozepal <afrozenajam@gmail.com>
2025-10-07 14:07:14 +0500

    fix:responisve

--- src/components/ui/GraphModal.jsx
+++ src/components/ui/GraphModal.jsx
@@ -280,7 +280,7 @@ const GraphModal = ({
       }
       transition={{ duration: 0.5, ease: "easeInOut" }}
       // className="fixed max-w-[96vw] max-h-[90vh] overflow-y-auto bottom-0 right-2 xl:right-[5%] 2xl:right-[15%] flex items-center justify-center z-50 p-4 bg-white border border-gray-200 shadow-2xl rounded-t-2xl"
-      className="fixed bottom-0 w-[98%] md::w-[95%] xl:w-[85%] 2xl:w-[75%] right-1 md:right-[2%] xl:right-[5%] z-50 p-2 overflow-y-auto"
+      className="fixed bottom-0 w-[98%] md:w-[95%] lg:w-[70%] 2xl:w-[75%] right-1 md:right-[2%] xl:right-[5%] z-50 p-2 overflow-y-auto"
     >
     <div className="bg-white rounded-2xl w-full max-h-[90vh] overflow-hidden flex flex-col">
         {/* Header */}
