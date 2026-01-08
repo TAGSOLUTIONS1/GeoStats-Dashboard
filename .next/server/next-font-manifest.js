5345501e74d04169da12754bca4122a20e6570e9
afrozepal <afrozenajam@gmail.com>
2026-01-08 15:01:41 +0500

    Remove .next directory from git tracking

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/dev/Documents/work/GeoStats-Dashboard/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
